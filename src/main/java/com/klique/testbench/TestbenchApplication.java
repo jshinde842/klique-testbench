c1b9713113bff6b1c9d0dd5014d3b3bacf08126c
Jay Shinde <46733906+jshinde842@users.noreply.github.com>
2024-11-22 17:51:57 -0800

    Add files via upload

---
+++ src/main/java/com/klique/testbench/TestbenchApplication.java
@@ -0,0 +1,13 @@
+package com.klique.testbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestbenchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klique/testbench/TestbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klique.testbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
